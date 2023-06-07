e15a76bc6374cf20288e2652282e9ca7b57e0e1a
Raul Perez <ghoulrul@gmail.com>
2023-06-07 09:24:50 -0600

    Se corrigen los parametros de la coneccion

--- acupuntura/acupuntura/settings.py
+++ acupuntura/acupuntura/settings.py
@@ -74,12 +74,12 @@ WSGI_APPLICATION = "acupuntura.wsgi.application"
 
 DATABASES = {
     'default': {        
-        "ENGINE": os.environ.get("EVENTOS_SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("EVENTOS_SQL_DATABASE", BASE_DIR / "db.sqlite3"),
-        "USER": os.environ.get("EVENTOS_SQL_USER", "user"),
-        "PASSWORD": os.environ.get("EVENTOS_SQL_PASSWORD", "password"),
-        "HOST": os.environ.get("EVENTOS_SQL_HOST", "localhost"),
-        "PORT": os.environ.get("EVENTOS_SQL_PORT", "5432"),
+        "ENGINE": os.environ.get("ACUPUNTURA_SQL_ENGINE", "django.db.backends.sqlite3"),
+        "NAME": os.environ.get("ACUPUNTURA_SQL_DATABASE", BASE_DIR / "db.sqlite3"),
+        "USER": os.environ.get("ACUPUNTURA_SQL_USER", "user"),
+        "PASSWORD": os.environ.get("ACUPUNTURA_SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("ACUPUNTURA_SQL_HOST", "localhost"),
+        "PORT": os.environ.get("ACUPUNTURA_SQL_PORT", "5432"),
     }
 }
 
