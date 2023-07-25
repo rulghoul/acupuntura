8dcb60100b5d04ed7c20dfe1c3da9d833f4a86ce
Raul Perez <ghoulrul@gmail.com>
2023-07-25 17:40:54 -0600

    Se agregan variables de entorno

--- deployApp.sh
+++ deployApp.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+# Variables Acupuntura
+export ACUPUNTURA_SQL_ENGINE="django.db.backends.mysql"
+export ACUPUNTURA_SQL_DATABASE="salud"
+export ACUPUNTURA_SQL_USER="acupuntura"
+export ACUPUNTURA_SQL_PASSWORD="acupunturadjango"
+export ACUPUNTURA_SQL_HOST="127.0.0.1"
+export ACUPUNTURA_SQL_PORT="3306"
+
+
 #Crear entorno virtual y descargar las librerias
 python3 -m venv virtual
 source virtual/bin/activate
+
+# Variables Acupuntura
+export ACUPUNTURA_SQL_ENGINE
+export ACUPUNTURA_SQL_DATABASE
+export ACUPUNTURA_SQL_USER
+export ACUPUNTURA_SQL_PASSWORD
+export ACUPUNTURA_SQL_HOST
+export ACUPUNTURA_SQL_PORT
 
 python3 variables.py
 
