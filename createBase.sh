7f680caed9b1abdb041941381dd62d8877d3b49d
Raul Perez <ghoulrul@gmail.com>
2023-07-25 17:34:53 -0600

    Se actualiza script de creacion de la base de datos

--- createBase.sh
+++ createBase.sh
@@ -6,6 +6,12 @@
 DB_HOST="localhost"
 DB_USER="root"
 DB_PASSWORD=""
+
+ACUPUNTURA_SQL_DATABASE="salud"
+ACUPUNTURA_SQL_USER="acupuntura"
+ACUPUNTURA_SQL_PASSWORD="acupunturadjango"
+DB_HOST="localhost"
+
 echo "Ingrese la contraseña para la conexión a la base de datos:"
 
 
@@ -14,6 +20,9 @@ read -s DB_PASSWORD
 SQL2="CREATE DATABASE IF NOT EXISTS $ACUPUNTURA_SQL_DATABASE;"
 SQL2+="GRANT ALL PRIVILEGES ON $ACUPUNTURA_SQL_DATABASE.* TO '$ACUPUNTURA_SQL_USER'@'$DB_HOST' IDENTIFIED BY '$ACUPUNTURA_SQL_PASSWORD';"
 
-echo "Bases de datos y usuarios creados correctamente."
+
 
 mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e "$SQL2" || exit 1
+
+
+echo "Bases de datos y usuarios creados correctamente."
