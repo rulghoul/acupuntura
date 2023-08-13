dc3de5b0f774da18d05034be5ae1b2bee8204cab
Raul Perez <ghoulrul@gmail.com>
2023-08-13 11:21:21 -0600

    Se agrego validacion a los formularios del punto para poder guardar elemento por elemento

--- acupuntura/acupuntura/settings.py
+++ acupuntura/acupuntura/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-8%ql#_^_44wy^fdnc9sk2a@0sdvjesgbmy%)yj)j55kpoy*h0!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
